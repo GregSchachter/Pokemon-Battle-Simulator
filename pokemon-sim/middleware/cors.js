ce71f832523c3e5a0d7d327ef0fcadb431fef4ba
GregSchachter <gregschachter@gmail.com>
2026-01-24 09:57:43 -0500

    Update cors.js

--- pokemon-sim/middleware/cors.js
+++ pokemon-sim/middleware/cors.js
@@ -1,7 +1,10 @@
 const cors = require("cors");
 
 var corsOptions = {
-  origin: "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    "https://pokemon-battle-simulator-c3n7.onrender.com",
+  ],
   allowedHeaders: ["Content-Type", "Authorization"],
   methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
